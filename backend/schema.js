96a5a32eca9567d787a652b3da88e9776da0723f
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-11-26 21:53:43 -0500

    Setup and test GraphQL

---
+++ backend/schema.js
@@ -0,0 +1,38 @@
+const {
+  GraphQLObjectType,
+  GraphQLList,
+  GraphQLString,
+  GraphQLSchema,
+} = require("graphql");
+
+const TestType = new GraphQLObjectType({
+  name: "Test",
+  fields: () => ({
+    test_field: { type: GraphQLString },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    test: {
+      type: TestType,
+      resolve() {
+        return { test_field: "Cool beans!" };
+      },
+    },
+    repeat: {
+      type: TestType,
+      args: {
+        word: { type: GraphQLString },
+      },
+      resolve(_, { word }) {
+        return { test_field: word };
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
