0c74c34e1c86484ab153700c7a7b43fd20907d19
Shai Gilboa <shizel@gmail.com>
2020-11-28 16:07:06 -0500

    apollo client setup

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,24 @@
+import { gql } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
+
 import '../styles/globals.css'
 
+import ApolloClient from '../apollo';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  ApolloClient.query({
+    query: gql`
+      query {
+        test
+      }
+    `
+  })
+  .then((res)=>console.log('res',res))
+  return (
+    <ApolloProvider client={ApolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
