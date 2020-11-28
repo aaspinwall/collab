8a3ee86d8a6a6d91987347eb2093cf83b7626ccb
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-11-28 14:50:31 -0500

    Refactor GraphQL schema

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
-const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./schema');
-const resolvers = require('./resolvers/resolvers.root');
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema/schema.root");
+const resolvers = require("./resolvers/resolvers.root");
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 // The `listen` method launches a web server.
 server.listen().then(({ url }) => {
