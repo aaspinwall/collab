d3a700ae071b1d693f526bba7d7c9a817016e31e
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-12-05 18:11:16 -0500

    This should break it

--- backend/resolvers/resolvers.root.js
+++ backend/resolvers/resolvers.root.js
@@ -1,16 +1,16 @@
 const UsersQuery = require('./users/users.query');
 const UsersResolver = require('./users/users.resolver');
-const TestQuery = require('./test.query');
+const TestQuery = require('./test.query')
 
 const resolvers = {
   Query: {
-    ...UsersQuery,
-    ...TestQuery,
+    ...UsersQuery
+    ...TestQuery
   },
 
   Mutation: {
-    ...UsersResolver,
+    ...UsersResolver
   },
-};
+}
 
-module.exports = resolvers;
+module.exports = resolvers
