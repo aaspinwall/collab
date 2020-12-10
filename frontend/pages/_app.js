f0743f2b570077a33d8e983ebae5a71bbf5dcfd4
Andrew Baldwin <abald.cg.ide@aldogroup.com>
2020-12-09 20:12:24 -0500

    Remove deprecated query from front-end

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,26 +1,14 @@
-import { gql } from '@apollo/client';
 import { ApolloProvider } from '@apollo/client';
 
 import '../styles/globals.css'
 
 // this imports the "client"- which is the connection
-// to the BE 
+// to the BE
 // (at the moment the be is "localhost", when we will
 // have it hosted we can change it)
 import ApolloClient from '../apollo';
 
 function MyApp({ Component, pageProps }) {
-  
-  // test query without hooks
-  // the response is console.logged
-  ApolloClient.query({
-    query: gql`
-      query {
-        test
-      }
-    `
-  })
-  .then((res)=>console.log('res',res))
   return (
     // so that we can make the calls anywhere in the app
     // we wrap it in a provider.
