44973b14c82d426d87d385f5ccce8c7695629fe4
Shai Gilboa <shizel@gmail.com>
2020-12-13 23:32:54 -0500

    and now???!!???!!!

--- backend/routes/voters/voters.test.js
+++ backend/routes/voters/voters.test.js
@@ -19,24 +19,29 @@ const graphqlTestCall = require('../../tests/graphqlTestCall');
 //   }
 // `;
 
-// describe('Voters Resolvers', () => {
-//   it('adds a new voter', async () => {
-//     const {
-//       data: {
-//         addVoter: {
-//           voter: { name, email },
-//           code,
-//           success,
-//         },
-//       },
-//     } = await graphqlTestCall(addVoterMutation, {
-//       name: 'Alright',
-//       email: 'freddy@notkreuger.com',
-//     });
+describe('Voters Resolvers', () => {
+  it('adds a new voter', async () => {
+    // const {
+    //   data: {
+    //     addVoter: {
+    //       voter: { name, email },
+    //       code,
+    //       success,
+    //     },
+    //   },
+    // } = await graphqlTestCall(addVoterMutation, {
+    //   name: 'Alright',
+    //   email: 'freddy@notkreuger.com',
+    // });
 
-//     expect(code).toEqual('200');
-//     expect(success).toBe(true);
-//     expect(name).toEqual('Alright');
-//     expect(email).toEqual('freddy@notkreuger.com');
-//   });
-// });
+    // expect(code).toEqual('200');
+    // expect(success).toBe(true);
+    // expect(name).toEqual('Alright');
+    // expect(email).toEqual('freddy@notkreuger.com');
+    
+    const one = 1
+    /**--- --- filler should be deleted */
+    expect(one).toEqual(1);
+  });
+});
+
