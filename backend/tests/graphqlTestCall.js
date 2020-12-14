e630742cf3d020fa5b2899c5ab3aa1055c82c9ff
Shai Gilboa <shizel@gmail.com>
2020-12-13 23:05:15 -0500

    now?

--- backend/tests/graphqlTestCall.js
+++ backend/tests/graphqlTestCall.js
@@ -1,15 +1,14 @@
 // Check this file... resolvers.root doesn't exist
 
-/* const { graphql } = require('graphql');
+const { graphql } = require('graphql');
 const { makeExecutableSchema } = require('graphql-tools');
 
-const typeDefs = require('../schema/schema.root');
+const typeDefs = require('../routes/schema.root.js');
 
-const resolvers = require('../resolvers/resolvers.root');
+const resolvers = require('../routes/resolvers.root.js');
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
 module.exports = async function graphqlTestCall(query, variables) {
   return graphql(schema, query, undefined, {}, variables);
 };
- */
