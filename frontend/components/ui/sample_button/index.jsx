efdcf593b06b47bf4203b711a96b08b79ba8bb36
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-12-16 20:11:46 -0500

    add onClick and styles prop

--- frontend/components/ui/sample_button/index.jsx
+++ frontend/components/ui/sample_button/index.jsx
@@ -3,13 +3,14 @@ import Button from "./elements";
 
 //Styles live in the elements file
 
-const SampleButton = ({ children, ...props }) => {
+const SampleButton = ({ children, onClick, styles, ...props }) => {
   // Props
   // children is what goes inside the component
   // onClick is the event that fires when we click the component
+  // STYLES is the name of the prop passed and STYLE is the attribute to style a component
   // ...props is the rest of the props we might want to pass to the component
   return (
-    <Button {...props} onClick={props.onClick}>
+    <Button {...props} onClick={onClick} style={styles}>
       {children}
     </Button>
   );
