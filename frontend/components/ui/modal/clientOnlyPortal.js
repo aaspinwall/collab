131e8f033aabd5c3fc53a91fb952268d6d5937a4
Carlos Fabian <carl.fab.g@gmail.com>
2020-12-23 12:09:05 -0600

    code formatting with prettier

--- frontend/components/ui/modal/clientOnlyPortal.js
+++ frontend/components/ui/modal/clientOnlyPortal.js
@@ -1,21 +1,21 @@
-import { useRef, useEffect, useState } from 'react';
-import { createPortal } from 'react-dom';
+import { useRef, useEffect, useState } from "react";
+import { createPortal } from "react-dom";
 
 const ClientOnlyPortal = ({ children, selector }) => {
-    const ref = useRef()
-    const [mounted, setMounted] = useState(false)
-  
-    useEffect(() => {
+  const ref = useRef();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
     let container;
-    const rootContainer = document.createElement('div');
-    const parentElem = document.querySelector('#__next');
+    const rootContainer = document.createElement("div");
+    const parentElem = document.querySelector("#__next");
     parentElem.appendChild(rootContainer);
     container = rootContainer;
-      ref.current = container;
-      setMounted(true)
-    }, [selector]);
-  
-    return mounted ? createPortal(children, ref.current) : null;
-  }
+    ref.current = container;
+    setMounted(true);
+  }, [selector]);
 
-  export default ClientOnlyPortal;
+  return mounted ? createPortal(children, ref.current) : null;
+};
+
+export default ClientOnlyPortal;
