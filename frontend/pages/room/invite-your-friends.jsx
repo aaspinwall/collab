cec6cc5b499d85f9b3a62f5f2d3712bf94cfc3da
AlexandreGagnon-Lalonde <alexandregagnonlalonde@gmail.com>
2020-12-29 21:47:57 -0500

    styling and copy to clipboard

--- frontend/pages/room/invite-your-friends.jsx
+++ frontend/pages/room/invite-your-friends.jsx
@@ -1,31 +1,60 @@
-import React from "react";
-import Link from "next/link";
+import React, { useRef, useState } from "react";
 import styled from "styled-components";
 import Head from "next/head";
 import { COLORS } from "../../styles/colors";
+import SocialMediaSharing from "../../components/SocialMediaSharing";
+import Button from "../../components/ui/sample_button";
+import { GoToVotingStyles, CopyLinkStyles } from "../../styles/button";
 
 export default function InviteYourFriends() {
+  const [copySuccess, setCopySuccess] = useState(false);
+  const textToCopyRef = useRef(null);
+
+  const copyToClipboard = (ev) => {
+    ev.preventDefault();
+
+    const newTextArea = document.createElement("textarea");
+
+    newTextArea.value = textToCopyRef.current.innerText;
+
+    document.body.appendChild(newTextArea);
+
+    newTextArea.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(newTextArea);
+
+    setCopySuccess(true);
+  };
+
   return (
     <Container>
       <Head>
         <title>Invite Your Friends!</title>
       </Head>
       <FormContainer>
-        <Header>Invite Your Friends</Header>
-        <p>Link</p>
-        <button>Copy</button>
-        <p>or</p>
-        <p>Share to social media</p>
-        <SocialMediaContainer>
-          <Link href={"/"}>Instagram</Link>
-          <Link href={"/"}>Facebook</Link>
-          <Link href={"/"}>Twitter</Link>
-          <Link href={"/"}>LinkedIn</Link>
-          <Link href={"/"}>What's App</Link>
-          <Link href={"/"}>Snapchat</Link>
-          <Link href={"/"}>Reddit</Link>
-        </SocialMediaContainer>
-        <button>Go To Voting</button>
+        <Header>
+          Invite Your
+          <br />
+          Friends
+        </Header>
+        <GeneratedLink ref={textToCopyRef}>Link</GeneratedLink>
+        <Button
+          children={copySuccess ? "Copied!" : "Copy"}
+          styles={CopyLinkStyles}
+          onClick={copyToClipboard}
+        />
+        <GenericMessage>
+          or
+          <br />
+          share to social media
+        </GenericMessage>
+        <SocialMediaSharing />
+        <Button
+          children={"Go To Voting"}
+          styles={GoToVotingStyles} /* onClick={} */
+        />
       </FormContainer>
     </Container>
   );
@@ -47,31 +76,35 @@ const Container = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
-  .overflow {
-      width: 200px;
-      max-height: 100px;
-      overflow: auto;
-      list-style-type: none;
-      border: 1px solid black;
-      border-radius: 8px;
-      box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.3);
-      padding: 5px 8px;
-      text-align: center;
-    };
-  };
-  .each-option {
-      border-bottom: 1px solid black;
-      &:last-child {
-      border-bottom: none;
-    };
-  };
-  `;
+`;
 
 const Header = styled.h1`
   color: ${COLORS.SHADES.BLACK};
   text-align: center;
-  margin-top: 0;
+  margin: 0 0 30px;
   padding-top: 15px;
   font-size: 2rem;
+  text-transform: uppercase;
 `;
-const SocialMediaContainer = styled.div``;
+const GeneratedLink = styled.p`
+  text-align: center;
+  font-weight: bold;
+  font-size: 1.5em;
+  border-radius: 20px;
+  width: 100%;
+  height: 40px;
+  margin: 20px 0 10px 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: rgba(50, 50, 105, 0.15) 0px 2px 5px 0px,
+    rgba(0, 0, 0, 0.05) 0px 1px 1px 0px;
+  background-color: ${COLORS.SHADES.OFFWHITE};
+  color: ${COLORS.SHADES.BLACK};
+  transition: all 0.2s ease-in-out;
+`;
+const GenericMessage = styled.p`
+  text-align: center;
+  font-weight: bold;
+  margin-top: 20px;
+`;
