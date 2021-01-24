c1820df602985ecc4648a84bd827939f6345d372
kolbymckeown <mckeownkolby@gmail.com>
2021-01-24 11:10:01 -0500

    Reroute input room index

--- frontend/pages/room/voting-room/index.js
+++ frontend/pages/room/voting-room/index.js
@@ -1,7 +1,19 @@
 import Head from "next/head";
+import { useEffect, useState, createRef } from "react";
+import Link from "next/link";
 import Card from "../../../components/ui/card";
 
 export default function VotingRoomEmpty() {
+  const [roomID, setRoomID] = useState("");
+  const onChange = (e) => {
+    e.persist();
+    setRoomID(e.target.value);
+  };
+
+  useEffect(() => {
+    console.log(roomID);
+  }, [roomID]);
+
   return (
     <Card>
       <Head>
@@ -10,6 +22,12 @@ export default function VotingRoomEmpty() {
       <p>
         This index.js page handles the route if no room ID was passed to the URL
       </p>
+      <div>
+        <input type="text" onChange={onChange} />
+        <Link href={`voting-room/${roomID}`}>
+            <a>Take me to the room!</a>
+          </Link>
+      </div>
     </Card>
   );
 }
