43bf9dbaddd70752afc007e0f2196b8689678a3c
kolbymckeown <mckeownkolby@gmail.com>
2021-01-24 11:14:45 -0500

    testing

--- frontend/pages/room/voting-room/index.js
+++ frontend/pages/room/voting-room/index.js
@@ -9,7 +9,7 @@ export default function VotingRoomEmpty() {
     e.persist();
     setRoomID(e.target.value);
   };
-
+  // remove useEffect before prod
   useEffect(() => {
     console.log(roomID);
   }, [roomID]);
