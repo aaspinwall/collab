c564aadd7d359107e472be81942f0c57436e8695
Andrew Baldwin <andrew.baldwin44@gmail.com>
2021-01-25 22:09:29 -0500

    Delete room on room timeout

--- frontend/pages/room/voting-room/styled-voting.js
+++ frontend/pages/room/voting-room/styled-voting.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import styled from "styled-components";
-import { COLORS } from "../../../styles/colors";
 
+import { COLORS } from "../../../styles/colors";
 import Timer from "../../../components/timer";
 import VotingForm from "../../../components/VotingForm";
 
