c8065a5f64f6712c5d9cba3dc3e31a28a2c19e27
kolbymckeown <mckeownkolby@gmail.com>
2021-02-02 20:23:58 -0500

    missing roomID issue

--- frontend/pages/room/voting-room/index.js
+++ frontend/pages/room/voting-room/index.js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import { useEffect, useState, createRef } from "react";
 import Link from "next/link";
 import Card from "../../../components/ui/card";
+import styled from "styled-components";
+import { COLORS } from '../../../styles/colors'
 
 export default function VotingRoomEmpty() {
   const [roomID, setRoomID] = useState("");
@@ -9,25 +11,60 @@ export default function VotingRoomEmpty() {
     e.persist();
     setRoomID(e.target.value);
   };
-  // remove useEffect before prod
-  useEffect(() => {
-    console.log(roomID);
-  }, [roomID]);
 
   return (
-    <Card>
+    <>
       <Head>
         <title>Voting Room Not Found</title>
       </Head>
-      <p>
-        This index.js page handles the route if no room ID was passed to the URL
-      </p>
-      <div>
-        <input type="text" onChange={onChange} />
-        <Link href={`voting-room/${roomID}`}>
-          <a>Take me to the room!</a>
-        </Link>
-      </div>
-    </Card>
+      <Container>
+        <Card>
+          <h2>RoomID missing</h2>
+          <span>Please enter your Voting Room ID here</span>
+          <input type="text" onChange={onChange} />
+          <Link href={`voting-room/${roomID}`}>
+            <a>Submit</a>
+          </Link>
+        </Card>
+      </Container>
+    </>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  height: 60%;
+  justify-content: center;
+  flex-direction: column;
+  input {
+    padding: 5px 10px;
+    margin: 15px 0;
+  }
+  h2 {
+    font-size: 2rem;
+    margin: 15px 0 0;
+  }
+  span {
+    font-style: italic;
+    font-size: 1.5rem;
+  }
+  a {
+    border: 1px solid ${COLORS.PURPLES.MAIN};
+    border-radius: 8px;
+    background: ${COLORS.PURPLES.MAIN};
+    padding: 8px 16px;
+    text-align: center;
+    width: 50%;
+    margin: 0 auto;
+    color: white;
+    font-size: 15px;
+    letter-spacing: 1.5px;
+    box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.4);
+    &:active {
+      background-color: ${COLORS.PURPLES.LIGHT};
+      color: ${COLORS.PURPLES.MAIN};
+      transform: scale(0.95);
+    }
+
+  }
+`;
